1feba2bdac9991ca43996d90352b3971841e2022
shivika24 <43493690+shivika24@users.noreply.github.com>
2019-10-30 12:57:15 +0530

    Create top_view.c

---
+++ top_view.c
@@ -0,0 +1,28 @@
+void topView(struct node *root)
+{
+    int *a=(int *)calloc(100,sizeof(int));
+     int *b=(int *)calloc(100,sizeof(int));
+    for(int i=0;i<100;i++)
+    {
+        b[i]=100;
+        a[i]=100;
+    }
+    distance(root,a,b,0,0);
+    for(int i=0;i<100;i++)
+    {
+        if(b[i]!=100)
+        printf("%d ",b[i]);
+    }
+}
+void distance(struct node *root,int *a,int *b,int level,int dist)
+{
+    if(root==NULL)
+    return;
+    if(dist<0&&level<a[50+dist]||dist>=0&&level<a[50+dist])
+    {
+    a[50+dist]=level;
+    b[50+dist]=root->data;
+    }
+    distance(root->left,a,b,level+1,dist-1);
+    distance(root->right,a,b,level+1,dist+1);
+}
